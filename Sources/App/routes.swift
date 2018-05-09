6fb58a87a4bbd369c8695b9870767af6314bbff3
Will McGinty <mcgintw@gmail.com>
2018-05-08 21:34:09 -0500

    Update to new Auth RC. Ensure throwing continues.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,7 +7,8 @@ public func routes(_ router: Router) throws {
     
     let todoController = TodoController()
     let basicAuthMiddleware = User.basicAuthMiddleware(using: BCrypt)
-    let basicAuthGroup = router.grouped(basicAuthMiddleware)
+    let guardAuthMiddleware = User.guardAuthMiddleware()
+    let basicAuthGroup = router.grouped([basicAuthMiddleware, guardAuthMiddleware])
     router.get("todos", use: todoController.index)
     basicAuthGroup.post("todos", use: todoController.create)
     basicAuthGroup.delete("todos", Todo.parameter, use: todoController.delete)
